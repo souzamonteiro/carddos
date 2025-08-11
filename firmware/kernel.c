956c336eb3409450afacb93014effa51643873a3
Roberto Luiz Souza Monteiro <roberto@souzamonteiro.com>
2025-08-11 08:12:51 -0300

    Portuguese texts translated into English

--- firmware/kernel.c
+++ firmware/kernel.c
@@ -1,16 +1,16 @@
 #include <stdint.h>
 
-/* Syscalls do CardDOS */
+/* CardDOS syscalls. */
 #define SYS_PRINT 0x09
 #define SYS_EXIT  0x4C
 
 void syscall_handler(uint32_t syscall_num, uint32_t arg1, uint32_t arg2) {
     switch (syscall_num) {
         case SYS_PRINT:
-            display_print((char*)arg1);  // Implemente display_print()
+            display_print((char*)arg1);  // Implement display_print()
             break;
         case SYS_EXIT:
-            current_task_exit(arg1);     // Implemente task_exit()
+            current_task_exit(arg1);     // Implement task_exit()
             break;
     }
 }
@@ -20,15 +20,15 @@ void load_program(const char *filename, uint32_t addr) {
     if (fp) {
         fread((void*)addr, 1, file_size(fp), fp);
         fclose(fp);
-        ((void(*)(void))addr)();  // Executa o programa
+        ((void(*)(void))addr)();  // Execute the program.
     }
 }
 
 void kernel_main() {
-    /* Inicializa hardware */
+    /* Initialize hardware. */
     display_init();
     keyboard_init();
 
-    /* Carrega e executa o shell (CARDOM.COM) */
-    load_program("0:/CARDOM.COM", 0x1000);  // Implemente load_program()
+    /* Load and run the shell (CARDOM.COM). */
+    load_program("0:/CARDOM.COM", 0x1000);
 }
